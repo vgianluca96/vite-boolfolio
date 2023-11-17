eb1ad96e1cdd2082f7b261e70994fe19313bbd52
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-17 16:53:03 +0100

    implementata bozza pagination

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -12,18 +12,43 @@ export default {
     return {
       url: 'http://127.0.0.1:8000/api/projects',
       projects: null,
+      current_page: 1,
+      las_page: null,
     }
   },
   methods: {
+    apiCall(postApiPage) {
+      axios
+        .get(this.url, {
+          params: {
+            page: postApiPage
+          }
+        })
+        .then(response => {
+          this.projects = response.data.result.data
+          this.last_page = response.data.result.last_page
+          console.log(response.data)
+          console.log(response.data.result.current_page)
+        });
+    },
+    prevCall() {
+      if (this.current_page > 1) {
+        this.current_page--;
+        this.apiCall(this.current_page)
+        console.log(this.current_page);
+      }
+    },
+    nextCall() {
+      if (this.current_page < this.last_page) {
+        this.current_page++;
+        this.apiCall(this.current_page)
+        console.log(this.current_page);
+      }
+    }
+
   },
   mounted() {
-    axios
-      .get(this.url)
-      .then(response => {
-        console.log(response.data);
-        this.projects = response.data.result.data
-        console.log(this.projects);
-      });
+    this.apiCall(this.current_page);
   }
 }
 
@@ -38,6 +63,13 @@ export default {
     <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 py-4 g-2">
       <ProjectCard v-for="project in projects" :project="project" />
     </div>
+
+    <nav aria-label="Page navigation example py-2">
+      <ul class="pagination">
+        <li class="page-item"><button @click="prevCall()" class="btn btn-primary">Prev</button></li>
+        <li class="page-item"><button @click="nextCall()" class="btn btn-primary">Next</button></li>
+      </ul>
+    </nav>
   </div>
 </template>
 
