a041ef114cfe895a7de13e4c32dec79a00c23d0f
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-20 16:27:27 +0100

    importato js di bootstrap

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './style.css'
 import './assets/scss/app.scss'
 import App from './App.vue'
 import { router } from './router.js'
+import 'bootstrap/js/index.esm.js'
 
 createApp(App).use(router).mount('#app')
