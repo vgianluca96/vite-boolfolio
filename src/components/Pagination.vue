84d25415ab843a22835643c52b7ab977b8e334e1
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-20 20:29:06 +0100

    refactoring codice

---
+++ src/components/Pagination.vue
@@ -0,0 +1,54 @@
+<script>
+
+import { state } from '../state.js';
+
+export default {
+  name: 'Pagination',
+  data() {
+    return {
+      state,
+      current_page: 1,
+    }
+  },
+  methods: {
+    prevCall() {
+      if (this.current_page > 1) {
+        this.current_page--;
+        state.apiCall(this.current_page);
+      }
+    },
+    nextCall() {
+      if (this.current_page < state.last_page) {
+        this.current_page++;
+        state.apiCall(this.current_page);
+      }
+    },
+    pageCall(n) {
+      this.current_page = n;
+      state.apiCall(this.current_page);
+    }
+  },
+  mounted() {
+    state.apiCall(this.current_page);
+  }
+}
+
+</script>
+
+<template>
+  <nav aria-label="Page navigation example py-2">
+    <ul class="pagination">
+      <li class="page-item">
+        <a href="javascript:void(0)" class="page-link" @click="prevCall()">Prev</a>
+      </li>
+      <li class="page-item" :class="{ 'active': current_page == n }" v-for="n in state.last_page  ">
+        <a href="javascript:void(0)" class="page-link" @click="pageCall(n)">{{ n }}</a>
+      </li>
+      <li class="page-item">
+        <a href="javascript:void(0)" class="page-link" @click="nextCall()">Next</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style></style>
--- src/views/AppProjects.vue
+++ src/views/AppProjects.vue
@@ -1,17 +1,42 @@
 <script>
-import Projects from '../components/Projects.vue'
+
+import { state } from '../state.js';
+
+import ProjectCard from '../components/ProjectCard.vue';
+import Pagination from '../components/Pagination.vue';
 
 export default {
     name: 'AppProjects',
     components: {
-        Projects
+        ProjectCard,
+        Pagination
+    },
+    data() {
+        return {
+            state
+        }
     }
 }
 </script>
 
 <template>
     <main>
-        <Projects />
+        <div class="container py-4">
+            <h1>
+                All Projects
+            </h1>
+
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 py-4 g-2" v-if="state.projects">
+                <ProjectCard v-for="project in state.projects" :project="project" />
+            </div>
+            <div v-else>
+                <h3>
+                    Loading...
+                </h3>
+            </div>
+
+            <Pagination />
+        </div>
     </main>
 </template>
 
